fb16a806159f4cb52127da26a26c14a91ac307b9
Ian Umeda <github@ianumeda.com>
2014-07-22 23:57:03 -0700

    added working carousel to home, updated some plugins

--- wp-content/themes/roots/page-home.php
+++ wp-content/themes/roots/page-home.php
@@ -1,2 +1,4 @@
 <?php get_template_part('templates/page', 'header'); ?>
+foo
 <?php get_template_part('templates/content', 'home'); ?>
+
--- wp-content/themes/roots/templates/content-home.php
+++ wp-content/themes/roots/templates/content-home.php
@@ -1,25 +1,22 @@
 <?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    
+	<div class="entry-content">
 
-<?php
-$query = new WP_Query( array( 'post_type' => 'page', 'post_parent' => '2', 'posts_per_page' => '-1','order' =>'ASC' ) ); 
+<?php 
+$query = new WP_Query( array( 'post_type' => 'page', 'post_parent' => $post->ID, 'posts_per_page' => '999' ,'order' =>'ASC' ) ); 
 if ( $query->have_posts() ) {
-  $i=0;
-  $feature_buttons='<div id="feature_buttons" class="row hidden-xs hidden-sm">';
 ?>
 <div id="feature_box" class="row">
   <div id="feature_carousel" class="carousel carousel slide col-md-12" data-interval="20000">
-    <!-- Indicators -->
-    <ol class="carousel-indicators hidden-md hidden-lg">
-      <li data-target="#feature_carousel" data-slide-to="0" class="carousel-indicator active"></li>
-      <li data-target="#feature_carousel" data-slide-to="1" class="carousel-indicator"></li>
-      <li data-target="#feature_carousel" data-slide-to="2" class="carousel-indicator"></li>
-      <li data-target="#feature_carousel" data-slide-to="3" class="carousel-indicator"></li>
-    </ol>
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
 
 <?php	
+$slide_counter=0;
+$indicators='<!-- Indicators --><ol class="carousel-indicators hidden-md hidden-lg">';
 while ( $query->have_posts() ) { 
+  $indicators.='<li data-target="#feature_carousel" data-slide-to="'.$slide_counter.'" class="carousel-indicator '.($slide_counter==0 ? "active" : "").'"></li>';
   $query->the_post(); 
 	if (has_post_thumbnail( $post->ID ) ){
   $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
@@ -28,8 +25,8 @@ while ( $query->have_posts() ) {
 
 ?>      
 
-      <div class="item <?php if($i==0) echo " active"; ?>">
-        <img src="<?php echo $imgurl; ?>);">
+      <div class="item<?php if($slide_counter==0) echo " active"; ?>">
+        <img class="img-responsive" src="<?php echo $imgurl; ?>" alt="<?php echo $post->ID; ?>">
         <div class="carousel-caption ">
           <div class="carousel-item-title visible-xs"><a href="<?php echo get_permalink($post->ID); ?>"><?php echo get_the_title($post->ID); ?> </a></div>
           <div class="hidden-xs"><a href="<?php echo get_permalink($post->ID); ?>"><?php echo get_the_content($post->ID); ?></a></div>
@@ -37,6 +34,12 @@ while ( $query->have_posts() ) {
         </div>
         <div class="carousel_bottom">&nbsp;</div>
       </div>
+
+<?php 
+  $slide_counter++;
+}
+$indicators.='</ol>'; 
+?>
 
     </div>
 
@@ -48,10 +51,23 @@ while ( $query->have_posts() ) {
       <span class="glyphicon glyphicon-chevron-right"></span>
     </a>
   </div>
+<?php echo $indicators; ?>
 </div>
 <?php } else { ?>
-	<div class="alert alert-danger">Carousel content not found.</div>
+	<div class="alert alert-danger">Content not found.</div>
 <?php } wp_reset_postdata(); ?>
-  
+		
+  </div>
 
+
+	 
+	 
+	 
+	 
+	 
+  </article>
 <?php endwhile; ?>
+
+
+
+
