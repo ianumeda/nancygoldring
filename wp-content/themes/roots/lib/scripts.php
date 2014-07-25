034e945efb68fd3a405fc371abd99b71c88eff20
Ian Umeda <github@ianumeda.com>
2014-07-25 11:17:23 -0700

    switch carousel transition to fade, feature carousel caption and nag buttons styles

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ac1b6f8ad7eca205dd803d8855462846');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '06260f968042310880a317395d787273');
   // CSS for Bootstrap-Modal-Carousel
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
