e3a00ed07cdf5dedc98282c9263e26a45c1af906
Ian Umeda <github@ianumeda.com>
2014-07-25 16:58:39 -0700

    snazzy footer info hover feature

--- wp-content/themes/roots/templates/footer.php
+++ wp-content/themes/roots/templates/footer.php
@@ -1,8 +1,16 @@
-<footer class="content-info container" role="contentinfo">
+<footer class="content-info " role="contentinfo">
   <div class="row">
     <div class="col-lg-12">
+      <span id="footer_info_button" class="glyphicon glyphicon-info-sign"></span>
+    </div>
+  </div>
+  <div class="row reveal">
+    <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+        <div class="contact_info email"><span class="email_symbol">@</span><a href="mailto:nancy.goldring@gmail.com" target="_blank">nancy.goldring@gmail.com</a></div>
+        <div class="contact_info phone"><span class="glyphicon glyphicon-earphone"></span>(212) 123-4567</div>
+        <div class="contact_info envelope"><span class="glyphicon glyphicon-envelope"></span>463 West St., A1112, New York, NY 10014</div>
+      <div class="copyright"><span class="glyphicon glyphicon-copyright-mark"></span> <?php echo date('Y'); ?> <?php bloginfo('name'); ?></div>
     </div>
   </div>
 </footer>
