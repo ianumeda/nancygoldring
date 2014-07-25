c5e42cb132d4eafe843c64f31788aa7cbe8a84ee
Ian Umeda <github@ianumeda.com>
2014-07-24 23:28:40 -0700

    a whole bunch of style tweaks
    navbar branding, gradient backdrop, post and page column sizing

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d0ef0061316cd12c934167f34ffe151');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ac1b6f8ad7eca205dd803d8855462846');
   // CSS for Bootstrap-Modal-Carousel
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/roots/templates/page-header.php
+++ wp-content/themes/roots/templates/page-header.php
@@ -1,4 +1,4 @@
-<div class="page-header">
+<div class="page-header col-lg-8 col-lg-push-2 col-sm-10 col-sm-push-1">
   <h1>
     <?php echo roots_title(); ?>
   </h1>
