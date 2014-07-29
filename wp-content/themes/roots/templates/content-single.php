a8459a8eacfd424acaafa8c769021534edbd48ba
Ian Umeda <github@ianumeda.com>
2014-07-29 15:41:18 -0700

    re-configured page column sizing

--- wp-content/themes/roots/templates/content-single.php
+++ wp-content/themes/roots/templates/content-single.php
@@ -1,6 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <div class="col-lg-8 col-lg-push-2 col-sm-10 col-sm-push-1">
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
@@ -11,6 +10,5 @@
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
-  </div>
   </article>
 <?php endwhile; ?>
