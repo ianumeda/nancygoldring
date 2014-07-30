cb27f5b7a8d35eb21a3f2fbbd41c06ca4f0db429
Ian Umeda <github@ianumeda.com>
2014-07-29 17:00:32 -0700

    Merge branch 'david_dup2'

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f14e8559605be69028a8cb2e3ceecaf9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a3127147b871750f6504277ecceb893b');
   // CSS for Bootstrap-Modal-Carousel
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/roots/templates/content-page.php
+++ wp-content/themes/roots/templates/content-page.php
@@ -1,6 +1,4 @@
-<div class="col-lg-8 col-lg-push-2 col-sm-10 col-sm-push-1">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
-</div>
