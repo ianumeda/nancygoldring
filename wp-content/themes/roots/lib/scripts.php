001cb34300a1134719247efb6fbf7937e2ffb80f
Ian Umeda <github@ianumeda.com>
2014-07-31 16:56:29 -0700

    modal captions, opacity

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1c945f75bf58d7429a01c64bf5997a48');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f48962294eba89cb280332e0cfd4f373');
   // CSS for Bootstrap-Modal-Carousel
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
