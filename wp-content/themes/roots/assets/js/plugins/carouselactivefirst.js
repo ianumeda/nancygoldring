8882b10fb4ac3661349a25900b3931fca19b5f7a
Ian Umeda <github@ianumeda.com>
2014-08-01 15:26:14 -0700

    removed carouselactivefirst.js because it was too generic, made current work carousels work
    randomized carousel interval times between 8 and 16 seconds

--- wp-content/themes/roots/assets/js/plugins/carouselactivefirst.js
+++ wp-content/themes/roots/assets/js/plugins/carouselactivefirst.js
@@ -1,10 +1,10 @@
-$(function(){
-	$('.modal-item').first().addClass('active')
-});
-
-$(function(){
-	$('.carousel-item').first().addClass('active')
-});
-
-
-$(document).ready(function() { $('.carousel').each(function(){ $(this).carousel({ interval: 3000 }); }); });​
+// $(function(){
+//   $('.modal-item').first().addClass('active')
+// });
+//
+// $(function(){
+//   $('.carousel-item').first().addClass('active')
+// });
+//
+//
+// $(document).ready(function() { $('.carousel').each(function(){ $(this).carousel({ interval: 3000 }); }); });​
--- wp-content/themes/roots/templates/page-current-work.php
+++ wp-content/themes/roots/templates/page-current-work.php
@@ -1,7 +1,6 @@
 
 
-<div id="container" class="js-packery"
-  data-packery-options='{ "itemSelector": ".pack-item" }'>
+<div id="container" class="js-packery" data-packery-options='{ "itemSelector": ".pack-item" }'>
 	
 		<?php $loop = new WP_Query( array( 'post_type' => 'art', 'posts_per_page' => -1 ) ); ?>
 		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
@@ -11,27 +10,21 @@
 			
 			 <!-- POST CAROUSEL GALLERY -->  
 	 
-			 <div id="CurrentWorkCarousel<?php the_id() ?>" class="CurrentWorkCarousel carousel slide" data-ride="carousel">
+			 <div id="CurrentWorkCarousel<?php the_id() ?>" class="CurrentWorkCarousel carousel slide" data-ride="carousel" data-interval="<?php echo round(rand(8000000,16000000)/1000); ?>">
 		 
-		 
-		 
-		 
-		 
-			   
-
 			   <!-- Wrapper for slides -->
 			   <div class="carousel-inner">
 		   		<?php
-		   		        $images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) ); 
+ 		        $images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) ); 
 		   		/* $images is now a object that contains all images (related to post id 1) and their information ordered like the gallery interface. */
 		   		        if ( $images ) { 
+ 		                //looping through the images
+                    $number=-1;
+ 		                foreach ( $images as $attachment_id => $attachment ) {
+                      $number++;
+ 		                ?>
 
-		   		                //looping through the images
-		   		                foreach ( $images as $attachment_id => $attachment ) {
-		   		                ?>
-
-		   		                <?php /* Outputs the image like this: <img src="" alt="" title="" width="" height="" /> */  ?>							
-									<div class="carousel-item item">
+									<div class="carousel-item item<?php echo ($number==0 ? ' active' : ''); ?>">
    									
 								
 										<?php
@@ -49,14 +42,6 @@
 			     </div>
 
 			   </div>
-
-			   <!-- Controls -->
-			   <a class="left carousel-control" href="#CurrentWorkCarousel<?php the_id() ?>" role="button" data-slide="prev">
-			     <span class="glyphicon glyphicon-chevron-left"></span>
-			   </a>
-			   <a class="right carousel-control" href="#CurrentWorkCarousel<?php the_id() ?>" role="button" data-slide="next">
-			     <span class="glyphicon glyphicon-chevron-right"></span>
-			   </a>
 
 </div>
 			 <!-- /. POST CAROUSEL GALLERY -->
