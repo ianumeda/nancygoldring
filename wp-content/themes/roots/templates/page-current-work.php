7bfff9c2b9cb81899289b18f077e032cbb05fe54
davidmichelruddy <davidmichelruddy@gmail.com>
2014-08-01 08:51:56 -0700

    packers - div background image

--- wp-content/themes/roots/templates/page-current-work.php
+++ wp-content/themes/roots/templates/page-current-work.php
@@ -5,12 +5,17 @@
 	
 		<?php $loop = new WP_Query( array( 'post_type' => 'art', 'posts_per_page' => -1 ) ); ?>
 		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-	<div class="item">
+		
+		<?php $image_id = get_post_thumbnail_id();
+				$image_url = wp_get_attachment_image_src($image_id,'medium', true);
+				?>
+		<div class="item" style="background: url(<?php echo $image_url[0]; ?> ) !important;">
+		
 		<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-					<img src="<?php $image_id = get_post_thumbnail_id();
-		$image_url = wp_get_attachment_image_src($image_id,'thumbnail', true);
-		echo $image_url[0];  ?>" class="img-thumbnail">
 	</div>
 	
 	<?php endwhile; wp_reset_query(); ?>
 </div>
+
+
+
