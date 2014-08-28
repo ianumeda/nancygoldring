484ccc47a173334574b18f44902ef7bc5859d067
Ian Umeda <github@ianumeda.com>
2014-08-28 12:03:25 -0700

    fixed footer info, added "designed by ian"

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ef7f82e56f80d90f3f6ce7f97d5b2138');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a5fb098eae37a8dfeaceeb7008ebf1a2');
   // CSS for Bootstrap-Modal-Carousel
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/roots/templates/footer.php
+++ wp-content/themes/roots/templates/footer.php
@@ -5,12 +5,14 @@
     </div>
   </div>
   <div class="row reveal">
-    <div class="col-lg-12">
+    <div class="col-xs-12 col-sm-6">
       <?php dynamic_sidebar('sidebar-footer'); ?>
         <div class="contact_info email"><span class="email_symbol">@</span><a href="mailto:nancy.goldring@gmail.com" target="_blank">nancy.goldring@gmail.com</a></div>
-        <div class="contact_info phone"><span class="glyphicon glyphicon-earphone"></span>(212) 123-4567</div>
         <div class="contact_info envelope"><span class="glyphicon glyphicon-envelope"></span>463 West St., A1112, New York, NY 10014</div>
       <div class="copyright"><span class="glyphicon glyphicon-copyright-mark"></span> <?php echo date('Y'); ?> <?php bloginfo('name'); ?></div>
+    </div>
+    <div class="designed_by_ianumeda col-xs-12 col-sm-6">
+      Designed by <a href="http://ianumeda.com" target="_blank">Ian Umeda</a>
     </div>
   </div>
 </footer>
